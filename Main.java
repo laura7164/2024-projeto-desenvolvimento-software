f892b509057b105a637e95feab236f09621ae3f5
laura7164 <164506063+laura7164@users.noreply.github.com>
2024-06-12 19:31:23 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,13 +11,23 @@ public class Main {
         MenuCliente menuCliente = new MenuCliente();
         MenuFuncionario menuFuncionario = new MenuFuncionario();
         MenuEstoque menuEstoque = new MenuEstoque();
+        MenuAcao menuAcao = new MenuAcao();
+
+        List<Produto> estoque = new ArrayList<Produto>();
+
+        estoque.add(new Produto("Sabão em pó", "001", 33.16f, 100));
+        estoque.add(new Produto("Arroz", "002", 0.90f, 100));
+        estoque.add(new Produto("Feijão", "003", 8.72f, 100));
+        estoque.add(new Produto("Refrigerante", "004", 12.00f, 100));
+        estoque.add(new Produto("Salgadinho", "005", 6.50f, 100));
 
         do {
             System.out.println("Bem vindo ao mercado!");
             System.out.println("[1] Gerenciar funcionários");
-            System.out.println("[2] Gerenciar clientes e/ou compras");
-            System.out.println("[3] Gerenciar estoque de produtos");
-            System.out.println("[4] Sair");
+            System.out.println("[2] Gerenciar clientes");
+            System.out.println("[3] Gerenciar ações do funcionário");
+            System.out.println("[4] Gerenciar estoque de produtos");
+            System.out.println("[5] Sair");
 
             System.out.print("Escolha uma opção: ");
             opcao = leitor.nextInt();
@@ -28,16 +40,19 @@ public class Main {
                     menuCliente.executarMenuCliente();
                     break;
                 case 3:
-                    menuEstoque.executarMenuEstoque();
+                    menuAcao.executarMenuAcao();
                     break;
                 case 4:
+                    menuEstoque.executarMenuEstoque();
+                    break;
+                case 5:
                     System.out.println(">> Saindo do sistema...");
                     break;
                 default:
                     System.out.println(">> Opção inválida...");
                     break;
             }
-        } while(opcao != 4);
+        } while(opcao != 5);
 
     }
 }
