57459393139a768160e24ae08b56b291fc2d4e5b
laura7164 <164506063+laura7164@users.noreply.github.com>
2024-06-13 20:18:16 -0300

    Update Compra.java

--- Models/Compra.java
+++ Models/Compra.java
@@ -1,5 +1,62 @@
 package Models;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Compra {
-    
+
+    private int idCompra;
+    private int idFuncionario;
+    private List<Produto> produtos;
+    private float valorTotal;
+    private LocalDateTime dataHora;
+
+    public Compra(int idCompra, int idFuncionario, List<Produto> produtos, float valorTotal) {
+        this.idCompra = idCompra;
+        this.idFuncionario = idFuncionario;
+        this.produtos = produtos;
+        this.valorTotal = valorTotal;
+        this.dataHora = LocalDateTime.now();
+    }
+
+    public int getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(int idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public int getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(int idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(float valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
 }
