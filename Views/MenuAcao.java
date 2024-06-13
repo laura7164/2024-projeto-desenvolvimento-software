9cc5ebeef3bea6cc4eba45232f024360e1e233a3
laura7164 <164506063+laura7164@users.noreply.github.com>
2024-06-13 20:16:03 -0300

    Update MenuAcao.java

--- Views/MenuAcao.java
+++ Views/MenuAcao.java
@@ -1,10 +1,14 @@
 package Views;
 import java.util.Scanner;
 
+import Controllers.AcaoController;
+
 public class MenuAcao {
     
     Scanner leitor = new Scanner(System.in);
     private int opcao;
+
+    AcaoController acaoController = new AcaoController();
 
     public void executarMenuAcao() {
         do {
@@ -13,27 +17,26 @@ public class MenuAcao {
             System.out.println("[3] Bater ponto de entrada");
             System.out.println("[4] Bater ponto de saída");
             System.out.println("[5] Sair");
-            //quantidade produtos no estoque
 
             System.out.println("Escolha uma opção: ");
             opcao = leitor.nextInt();
 
             switch(opcao) {
                 case 1:
-                    
+                    acaoController.realizarCompra();
                     break;
                 case 2:
+                    acaoController.reporProduto();
                     break;
                 case 3:
+                    acaoController.baterPontoEntrada();
                     break;
-
                 case 4:
+                    acaoController.baterPontoSaida();
                     break;
-
                 case 5:
                     System.out.println(">> Saindo...");
                     break;
-
                 default:
                     System.out.println(">> Opção inválida...");
                     break;
