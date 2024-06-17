3e6ad0cb90fa3a661292bd380288794f39708a77
laura7164 <164506063+laura7164@users.noreply.github.com>
2024-06-17 20:38:47 -0300

    Update Gerente.java

--- Models/Gerente.java
+++ Models/Gerente.java
@@ -4,7 +4,7 @@ public class Gerente extends Funcionario {
 
     private String equipe;
 
-    public Gerente(String nome, int idFuncionario, String equipe) {
+    public Gerente(String nome, String idFuncionario, String equipe) {
         super(nome, idFuncionario);
         this.salario = 3000;
         this.equipe = equipe;
@@ -20,12 +20,12 @@ public class Gerente extends Funcionario {
 
     @Override
     public void exibirInfo() {
-        System.out.println("----------------------------");
+        System.out.println("===================================");
         System.out.println("Nome: " + getNome());
         System.out.println("Salário: " + getSalario());
         System.out.println("Id do funcionário: " + getIdFuncionario());
-        System.out.println("Equipe sob gerência: " + getEquipe());
-        System.out.println("----------------------------");
+        System.out.println("Equipe sob gerencia: " + getEquipe());
+        System.out.println("===================================");
     }
 
 }
