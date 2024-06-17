66dd9d79bd6ecf01b057fa5884262650f4255369
laura7164 <164506063+laura7164@users.noreply.github.com>
2024-06-17 20:39:59 -0300

    Update EstoqueController.java

--- Controllers/EstoqueController.java
+++ Controllers/EstoqueController.java
@@ -1,6 +1,7 @@
 package Controllers;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import Models.Produto;
@@ -11,66 +12,107 @@ public class EstoqueController {
     List<Produto> estoque = new ArrayList<Produto>();
 
     public void adicionarProduto(){
-        System.out.print("Digite o nome do produto: ");
-        String nome = leitor.nextLine();
+        try {
+            System.out.println(">> Adicionando um produto...");
 
-        System.out.print("Digite o código do produto: ");
-        String codigo = leitor.nextLine();
+            System.out.print("Digite o nome do produto: ");
+            String nome = leitor.nextLine();
 
-        System.out.print("Digite o preço do produto: ");
-        float preco = leitor.nextFloat();
+            System.out.print("Digite o código do produto: ");
+            String codigo = leitor.nextLine();
 
-        System.out.print("Digite a quantidade do produto: ");
-        int quantidade = leitor.nextInt();
+            System.out.print("Digite o preço do produto: ");
+            float preco = leitor.nextFloat();
 
-        Produto novoProduto = new Produto(nome, codigo, preco, quantidade);
-        estoque.add(novoProduto);
+            System.out.print("Digite a quantidade do produto: ");
+            int quantidade = leitor.nextInt();
+
+            leitor.nextLine();
+
+            Produto novoProduto = new Produto(nome, codigo, preco, quantidade);
+            estoque.add(novoProduto);
+
+            System.out.println(">> Produto adicionado com sucesso!");
+        } catch (InputMismatchException e) {
+            System.out.println(">> Erro: tipo de dado incorreto. Por favor, insira os dados corretamente.");
+            leitor.nextLine();
+        } catch (Exception e) {
+            System.out.println(">> Erro: " + e.getMessage());
+        }
     }
 
     public void listarProdutos(){
+        System.out.println("====== Lista de produtos =========");
         for(int i = 0; i < estoque.size(); i++){
-            ((Produto) estoque).verProduto();
+            ((Produto) estoque.get(i)).verProduto();
         }
     }
 
     public void removerProduto(){
+        System.out.println(">> Removendo um produto...");
+
         System.out.print("Digite o código do produto que deseja remover: ");
-        String codigoRemover = leitor.nextLine();
+        String codigoProdutoRemover = leitor.nextLine();
+
+        boolean encontrado = false;
 
         for(int i = 0; i < estoque.size(); i++){
-            if(codigoRemover.equals(estoque.get(i).getCodigo())){
+            if(codigoProdutoRemover.equals(estoque.get(i).getCodigo())){
                 estoque.remove(i);
-            }else{
-                System.out.println(">> Produto não encontrado!");
+                encontrado = true;
+                System.out.println(">> Produto removido com sucesso!");
+                break;
             }
         }
+
+        if(!encontrado)
+            System.out.println(">> Produto não encontrado!");
     }
 
     public void buscarProduto(){
+        System.out.println(">> Buscando um produto...");
+
         System.out.print("Digite o código do produto que deseja buscar: ");
-        String codigoBuscar = leitor.nextLine();
+        String codigoProdutoBuscar = leitor.nextLine();
+
+        boolean encontrado = false;
 
         for(int i = 0; i < estoque.size(); i++){
-            if(codigoBuscar.equals(estoque.get(i).getCodigo())){
-                ((Produto) estoque).verProduto();
-            }else{
-                System.out.println(">> Produto não encontrado!");
+            if(codigoProdutoBuscar.equals(estoque.get(i).getCodigo())){
+                ((Produto) estoque.get(i)).verProduto();
+                encontrado = true;
+                break;
             }
         }
+
+        if(!encontrado)
+            System.out.println(">> Produto não encontrado!");
     }
 
     public void atualizarPreco(){
+        System.out.println(">> Atualizando o preço de um produto...");
+
         System.out.print("Digite o código do produto que deseja atualizar: ");
-        String codigoAtualizar = leitor.nextLine();
+        String codigoProdutoAtualizar = leitor.nextLine();
+
+        boolean encontrado = false;
 
         System.out.print("Digite o novo preço do produto: ");
-        float novoPreco = leitor.nextFloat();
+        float novoPrecoProduto = leitor.nextFloat();
+
+        leitor.nextLine();
 
         for(int i = 0; i < estoque.size(); i++){
-            if(codigoAtualizar.equals(estoque.get(i).getCodigo())){
-                ((Produto) estoque).setPreco(novoPreco);
+            if(codigoProdutoAtualizar.equals(estoque.get(i).getCodigo())){
+                ((Produto) estoque.get(i)).setPreco(novoPrecoProduto);
+                encontrado = true;
+                System.out.println(">> Preço do produto atualizado com sucesso!");
+                break;
             }
         }
+
+        if(!encontrado)
+            System.out.println(">> Produto não encontrado");
     }
     
 }
