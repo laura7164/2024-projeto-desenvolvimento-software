a8a9ab8d6063b4eab421eb3b69137a7a85971c06
laura7164 <164506063+laura7164@users.noreply.github.com>
2024-06-17 20:36:00 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-import Models.Produto;
 import Views.MenuAcao;
 import Views.MenuCliente;
 import Views.MenuEstoque;
@@ -18,22 +15,15 @@ public class Main {
         MenuFuncionario menuFuncionario = new MenuFuncionario();
         MenuEstoque menuEstoque = new MenuEstoque();
         MenuAcao menuAcao = new MenuAcao();
-
-        List<Produto> estoque = new ArrayList<Produto>();
-
-        estoque.add(new Produto("Sabão em pó", "001", 33.16f, 100));
-        estoque.add(new Produto("Arroz", "002", 0.90f, 100));
-        estoque.add(new Produto("Feijão", "003", 8.72f, 100));
-        estoque.add(new Produto("Refrigerante", "004", 12.00f, 100));
-        estoque.add(new Produto("Salgadinho", "005", 6.50f, 100));
-
+        
         do {
-            System.out.println("Bem vindo ao mercado!");
+            System.out.println("====== Bem vindo ao mercado ======");
             System.out.println("[1] Gerenciar funcionários");
             System.out.println("[2] Gerenciar clientes");
             System.out.println("[3] Gerenciar ações do funcionário");
             System.out.println("[4] Gerenciar estoque de produtos");
             System.out.println("[5] Sair");
+            System.out.println("===================================");
 
             System.out.print("Escolha uma opção: ");
             opcao = leitor.nextInt();
