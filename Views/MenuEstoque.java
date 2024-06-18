4b24329f1b7987ccb3a023bb87e67f978bc56944
laura7164 <164506063+laura7164@users.noreply.github.com>
2024-06-18 17:01:42 -0300

    Update MenuEstoque.java

--- Views/MenuEstoque.java
+++ Views/MenuEstoque.java
@@ -12,12 +12,15 @@ public class MenuEstoque {
 
     public void executarMenuEstoque() {
         do {
+            System.out.println("===================================");
             System.out.println("[1] Adicionar produto");
             System.out.println("[2] Listar produtos");
             System.out.println("[3] Remover produto");
-            System.out.println("[4] Atualizar preço do produto");
+            System.out.println("[4] Atualizar produto");
             System.out.println("[5] Buscar produto");
-            System.out.println("[6] Sair");
+            System.out.println("[6] Salvar produtos em arquivo");
+            System.out.println("[7] Sair");
+            System.out.println("===================================");
 
             System.out.print("Escolha uma opção: ");
             opcao = leitor.nextInt();
@@ -33,19 +36,22 @@ public class MenuEstoque {
                     estoqueController.removerProduto();
                     break;
                 case 4:
-                    estoqueController.atualizarPreco();
+                    estoqueController.atualizarProduto();
                     break;
                 case 5:
                     estoqueController.buscarProduto();
                     break;
                 case 6:
-                    System.out.println(">> Saindo do sistema...");
+                    estoqueController.salvarProdutos();
+                    break;
+                case 7:
+                    System.out.println(">> Voltando ao menu principal...");
                     break;
                 default:
                     System.out.println(">> Opção inválida...");
                     break;
             }
-        } while(opcao != 6);
+        } while(opcao != 7);
         
     }
 }
