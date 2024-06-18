225572ed81ef9ca083f4d224e93d4e26d0a4a862
laura7164 <164506063+laura7164@users.noreply.github.com>
2024-06-18 16:59:40 -0300

    Update EstoqueController.java

--- Controllers/EstoqueController.java
+++ Controllers/EstoqueController.java
@@ -1,5 +1,8 @@
 package Controllers;
 import java.util.List;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -20,6 +23,11 @@ public class EstoqueController {
 
             System.out.print("Digite o código do produto: ");
             String codigo = leitor.nextLine();
+
+            if (existeCodigo(codigo)) {
+                System.out.println(">> Erro: Já existe um produto com esse código.");
+                return;
+            }
 
             System.out.print("Digite o preço do produto: ");
             float preco = leitor.nextFloat();
@@ -89,30 +97,72 @@ public class EstoqueController {
             System.out.println(">> Produto não encontrado!");
     }
 
-    public void atualizarPreco(){
-        System.out.println(">> Atualizando o preço de um produto...");
+    public void atualizarProduto(){
+        System.out.println(">> Atualizando um produto...");
 
         System.out.print("Digite o código do produto que deseja atualizar: ");
         String codigoProdutoAtualizar = leitor.nextLine();
 
         boolean encontrado = false;
 
-        System.out.print("Digite o novo preço do produto: ");
-        float novoPrecoProduto = leitor.nextFloat();
-
-        leitor.nextLine();
-
         for(int i = 0; i < estoque.size(); i++){
             if(codigoProdutoAtualizar.equals(estoque.get(i).getCodigo())){
-                ((Produto) estoque.get(i)).setPreco(novoPrecoProduto);
                 encontrado = true;
-                System.out.println(">> Preço do produto atualizado com sucesso!");
-                break;
+
+                System.out.println("===================================");
+                System.out.println("[1] Preço");
+                System.out.println("[2] Quantidade");
+                System.out.println("===================================");
+
+                System.out.print("Escolha uma opção: ");
+                int opcao = leitor.nextInt();
+
+                switch(opcao){
+                    case 1:
+                        leitor.nextLine();
+                        System.out.print("Digite o novo preço: ");
+                        float novoPreco = leitor.nextFloat();
+
+                        ((Produto) estoque.get(i)).setPreco(novoPreco);
+                        System.out.println(">> Preço atualizado com sucesso!");
+                        break;
+                    case 2:
+                        leitor.nextLine();
+                        System.out.print("Digite a nova quantidade: ");
+                        int novaQuantidade = leitor.nextInt();
+
+                        ((Produto) estoque.get(i)).setQuantidade(novaQuantidade);
+                        System.out.println(">> Quantidade atualizada com sucesso!");
+                        break;
+                    default:
+                        System.out.println(">> Opção inválida...");
+                        break;
+                }
             }
         }
 
+        salvarProdutos();
+
         if(!encontrado)
-            System.out.println(">> Produto não encontrado");
+            System.out.println(">> Produto não encontrado!");
+    }   
+
+    public void salvarProdutos() {
+        try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("estoque.save"))) {
+            os.writeObject(estoque);
+            System.out.println(">> Produtos salvos com sucesso!");
+        } catch (IOException e) {
+            System.out.println(">> Erro ao salvar produtos: " + e.getMessage());
+        }
+    }
+
+     private boolean existeCodigo(String codigo) {
+        for (Produto produtos : estoque) {
+            if (produtos.getCodigo().equals(codigo)) {
+                return true;
+            }
+        }
+        return false;
     }
     
 }
